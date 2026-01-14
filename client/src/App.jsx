87897b82afb36ff114bc3c09b6d881e9cb29a21b
Kunal Rathore <135549137+koderrathore@users.noreply.github.com>
2026-01-14 15:53:28 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
   );
   const dispatch = useDispatch();
   useEffect(() => {
-    const token = JSON.parse(sessionStorage.getItem("token"));
+    const token = JSON.parse(sessionStorage.getItem("token")||null);
     dispatch(checkLogin(token))
       .then((data) => {
         console.log(data);
